f3a55d03d8fbf0c808cf02d55abccb98f775f2d4
Aishwarya <124514705+GOver1519@users.noreply.github.com>
2026-03-05 18:32:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/action')
+def action():
+    return render_template('action.html')
+
+@app.route('/adventure')
+def adventure():
+    return render_template('adventure.html')
+
+@app.route('/sports')
+def sports():
+    return render_template('sports.html')
+
+@app.route('/puzzle')
+def puzzle():
+    return render_template('puzzle.html')
+
+@app.route('/simulation')
+def simulation():
+    return render_template('simulation.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
