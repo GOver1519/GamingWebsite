69d0b9f6c45b6edbef97bba4a232d1eb74778c82
Aishwarya <124514705+GOver1519@users.noreply.github.com>
2026-03-05 18:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,5 @@ def simulation():
     return render_template('simulation.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
